1391baa81e094156f7d5e08f06969e3f39be95a4
Hacene YOUNSI <hacene.younsi@hotmail.com>
2018-11-14 22:58:03 +0100

    Initial commit

---
+++ app/src/main/java/com/systems/fennec/chatbot/Dashboard.java
@@ -0,0 +1,68 @@
+package com.systems.fennec.chatbot;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Dashboard extends Fragment
+{
+    ImageView btnBluetooth;
+
+    ImageView btnForward;
+    ImageView btnBackward;
+    ImageView btnRight;
+    ImageView btnLeft;
+
+    ImageView btnGetTemperature;
+    ImageView btnGetDistance;
+    ImageView btnLedControl;
+    ImageView btnLevelBattery;
+
+    TextView stateLeds;
+    TextView stateRightMotor;
+    TextView stateLeftMotor;
+
+    char command;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.activity_dashboard, null);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // Commande
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getContext(), "Avancer", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+
+        Toast.makeText(getContext(), "Dashboard", Toast.LENGTH_LONG).show();
+    }
+}
